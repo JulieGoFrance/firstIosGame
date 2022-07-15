861a530259704420a270b37e6383ea0f3a2071cf
Julie Go <julie.gosselin84@yahoo.fr>
2022-07-15 16:34:59 +0200

    Initial Commit

---
+++ CroixEtRonds/CroixEtRondsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CroixEtRondsApp.swift
+//  CroixEtRonds
+//
+//  Created by User on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CroixEtRondsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
