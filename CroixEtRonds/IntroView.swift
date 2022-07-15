42e7af487788f26ec13563d6d9d47f4eb87ba267
Julie Go <julie.gosselin84@yahoo.fr>
2022-07-15 16:37:32 +0200

    first Commit

---
+++ CroixEtRonds/IntroView.swift
@@ -0,0 +1,24 @@
+//
+//  IntroView.swift
+//  CroixEtRonds
+//
+//  Created by User on 15/07/2022.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+            }
+        }
+    }
+}
+
+struct IntroView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroView()
+    }
+}
